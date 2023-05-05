c724640435bbbb78643509fdbb81f3c9f85a01da
Keith Gaines <patrick.keith.gaines@gmail.com>
2023-05-05 09:18:33 -0500

    added ReadKey() to wait for keypress before exiting

--- bandNameGenerator/Program.cs
+++ bandNameGenerator/Program.cs
@@ -1,3 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
 
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Welcome to the Band Name Generator.");
+
+        Console.WriteLine("What's the name of the city you grew up in?");
+        string city = Console.ReadLine();
+
+        Console.WriteLine("What's the name of your pet?");
+        string pet = Console.ReadLine();
+
+        Console.WriteLine("Your band name could be " + city + " " + pet);
+
+        Console.WriteLine("Press any key to exit...");
+        Console.ReadKey();
+    }
+}
