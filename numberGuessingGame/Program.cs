886098445204d01b82997e9beadb1f2eeccf8b05
Keith Gaines <patrick.keith.gaines@gmail.com>
2023-05-05 15:37:56 -0500

    initial commit

---
+++ numberGuessingGame/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Welcome to the Number Guessing Game!");
+        Console.WriteLine("I'm thinking of a number between 1 and 100.");
+
+        Console.Write("Choose a difficulty. Type 'easy' or 'hard': ");
+        string difficulty = Console.ReadLine();
+
+        // chooses a random number from a range of 1 - 100 cast as an integer
+        Random random = new Random();
+        int chosen_number = random.Next(1, 100);
+
+        // if statement that assigns number of attempts based on difficulty
+        int attempts = difficulty == "easy" ? 10 : 5;
+        Console.WriteLine($"You have {attempts} attempts remaining to guess the number.");
+
+        // assigns user guess to variable 'guess' and casts it as an int from a string
+        Console.Write("Make a guess: ");
+        int guess = int.Parse(Console.ReadLine());
+
+        while (attempts > 0)
+        {
+            if (guess > chosen_number)
+            {
+                Console.Write("Too high. Guess again: ");
+                guess = int.Parse(Console.ReadLine());
+                attempts--;
+            }
+            else if (guess < chosen_number)
+            {
+                Console.Write("Too low. Guess again: ");
+                guess = int.Parse(Console.ReadLine());
+                attempts--;
+            }
+            else
+            {
+                Console.WriteLine("You guessed it");
+                attempts = 0;
+            }
+        }
+
+        if (attempts == 0)
+        {
+            Console.WriteLine($"The correct number was {chosen_number}. Game over");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ numberGuessingGame/README.md
@@ -0,0 +1,25 @@
+Sure, here's an updated README that only references the C# version:
+
+# Number Guessing Game
+
+This is a simple console-based number guessing game written in C#. The game generates a random number between 1 and 100, and the player must guess the number within a certain number of attempts.
+
+## Getting Started
+
+### Prerequisites
+
+- .NET Framework
+
+### Installation
+
+1. Clone the repository or download the code as a ZIP file.
+2. Open the solution file in Visual Studio.
+3. Build the project.
+4. Run the executable file.
+
+## How to Play
+
+1. When you start the game, you will be prompted to choose a difficulty level (easy or hard).
+2. The game will generate a random number between 1 and 100, and you will have a certain number of attempts to guess the number.
+3. After each guess, the game will tell you whether your guess was too high or too low.
+4. If you guess the number correctly within the allowed number of attempts, you win. Otherwise, you lose.
